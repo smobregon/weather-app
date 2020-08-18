b9b94a5d0b513a6dad228eb681fcaba59af44327
Savannah Obregon <savannah.obregon@lifeway.com>
2020-08-18 13:49:05 -0400

    update port

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require("./utils/forecast");
 
 const app = express();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
